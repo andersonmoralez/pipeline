a2702500b9b5ee96e29b2d481122934594b5cc53
unknown <anderson.moralez@hotmail.com>
2023-06-04 03:11:06 -0300

    feat: Implementing Contracts

--- src/stages/extract/extract_html_test.py
+++ src/stages/extract/extract_html_test.py
@@ -1,5 +1,7 @@
 from src.drivers.http_requester import HttpResquester
 from src.drivers.html_collector import HtmlCollector
+from src.stages.contracts.extract_contract import ExtractContract
+from src.errors.extract_error import ExtractError
 from .extract_html import ExtractHtml
 
 def test_extract():
@@ -11,3 +13,16 @@ def test_extract():
     res = extract_html.extract()
     print()
     print(res)
+
+    assert isinstance(res, ExtractContract)
+
+def test_extract_error():
+    http_requester = 'GerarErro'
+    html_collector = HtmlCollector()
+
+    extract_html = ExtractHtml(http_requester, html_collector)
+
+    try:
+        extract_html.extract()
+    except Exception as exception: # pylint: disable=broad-except
+        assert isinstance(exception, ExtractError)
